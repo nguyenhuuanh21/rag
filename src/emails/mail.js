318de46e47e4e669c0501dbec1bcd8d1c18d914a
nguyenhuuanh21 <nguyenhuuanh.user@gmail.com>
2026-05-26 15:43:52 +0700

    fix: parse mail env values correctly for nodemailer

--- src/emails/mail.js
+++ src/emails/mail.js
@@ -2,20 +2,20 @@ const nodemailer = require("nodemailer");
 const ejs = require('ejs');
 const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
-  port: process.env.MAIL_PORT,
-  secure: process.env.MAIL_SECURE,
+  port: Number(process.env.MAIL_PORT),
+  secure: process.env.MAIL_SECURE === "true",
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
   },
 });
 
-module.exports = async (template,payload) => {
-    const html=await ejs.renderFile(template,{payload:payload})
-    const info = await transporter.sendMail({
+module.exports = async (template, payload) => {
+  const html = await ejs.renderFile(template, { payload: payload })
+  const info = await transporter.sendMail({
     from: process.env.MAIL_FROM, // sender address
     to: payload.email,
     subject: payload.subject,
-    html:html, // HTML body
+    html: html, // HTML body
   });
 };
